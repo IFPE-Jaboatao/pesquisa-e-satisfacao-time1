fe71826c4ec8320273815452df1fa08e2d8e0f0e
Matheus Falcão <matheusfalcao695@gmail.com>
2026-05-04 23:02:40 -0300

    Criação do arquivo seeds para usuário

--- api/src/database/seeds/seed.module.ts
+++ api/src/database/seeds/seed.module.ts
@@ -4,9 +4,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { SeedService } from './seed.service';
 import { ProfilesSeed } from '../../modules/profiles/profiles.seed';
 import { Profile } from '../../modules/profiles/profiles.entity';
+import { UserSeeds } from 'src/modules/users/dtos/user.seeds';
+import { User } from 'src/modules/users/user.entity';
+import { ProfilesModule } from 'src/modules/profiles/profiles.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Profile])],
-  providers: [SeedService, ProfilesSeed],
+  imports: [TypeOrmModule.forFeature([Profile, User]), ProfilesModule],
+  providers: [SeedService, ProfilesSeed, UserSeeds],
 })
 export class SeedModule {}
