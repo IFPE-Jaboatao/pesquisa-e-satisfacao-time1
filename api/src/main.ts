da31754cdd475e71c09d5f0bf34802a1be20de74
josinon <josinon@gmail.com>
2026-05-09 11:03:47 -0300

    Update server listening address to '0.0.0.0' for external access

--- api/src/main.ts
+++ api/src/main.ts
@@ -32,7 +32,7 @@ async function bootstrap() {
 
   const port = process.env.PORT ?? 3000;
 
-  await app.listen(port);
+  await app.listen(port, '0.0.0.0');
 
   console.log('\n Server started!');
   console.log(` URL: http://localhost:${port}`);
